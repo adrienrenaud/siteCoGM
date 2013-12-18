56ffb23b5213d4dbfde18af37066c9c805262933
adrienrenaud <adrien.renaud@gmail.com>
2013-12-18 17:02:53 +0000

    save graph in siteCoGM/static

--- siteCoGM/settings.py
+++ siteCoGM/settings.py
@@ -129,5 +129,7 @@ STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
-    # os.path.join(BASE_DIR, 'siteCoGM/static'),
+    os.path.join(BASE_DIR, 'siteCoGM/static'),
 )
+
+# AWS_PRELOAD_METADATA = True
