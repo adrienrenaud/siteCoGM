1ae98d13d8b960d0126e0e4e43af8520ae6c9d8d
adrienrenaud <adrien.renaud@gmail.com>
2013-12-18 16:37:13 +0000

    debug off

--- siteCoGM/settings.py
+++ siteCoGM/settings.py
@@ -129,5 +129,5 @@ STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
-    os.path.join(BASE_DIR, 'siteCoGM/static'),
+    # os.path.join(BASE_DIR, 'siteCoGM/static'),
 )
