ccf8071d3fb27d4e302b38c0986ab38d55fea1bd
adrienrenaud <adrien.renaud@gmail.com>
2013-12-21 03:48:18 +0000

    S3 setup

--- siteCoGM/settings_dev.py
+++ siteCoGM/settings_dev.py
@@ -3,3 +3,12 @@ from siteCoGM.settings import *
 
 DEBUG = True
 TEMPLATE_DEBUG = True
+
+
+
+DEFAULT_FILE_STORAGE = 'django.core.files.storage.FileSystemStorage'
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'    
+
+
+MEDIA_URL = '/media/'
+STATIC_URL = '/static/'
